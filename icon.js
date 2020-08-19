bec7f63281daa2e991f551b80043a53395f516ee
dione-vianna <dioneg12@hotmail.com>
2020-08-19 16:41:54 -0300

    readme

---
+++ icon.js
@@ -0,0 +1,4 @@
+let a;
+const a = {};
+
+class Api {}
--- pages/UpdateUser.js
+++ pages/UpdateUser.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {
   View,
   YellowBox,
-  ScrollView,
+  ScrollVie,
   KeyboardAvoidingView,
   Alert,
 } from 'react-native';
